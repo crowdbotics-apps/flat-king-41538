e5c32cacc10b2bb51c841161c43258333df67907
Crowdbotics <team@crowdbotics.com>
2023-06-16 17:13:48 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "flat-king-41538",
+  url: "https://flat-king-41538.botics.co",
+  api: "https://flat-king-41538.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
